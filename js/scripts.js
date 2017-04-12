1d95258df49ce6e60e408559ee5adbced574b1a3
Robert Manolis <grobertpm@gmail.com>
2017-04-12 00:49:41 -0700

    added responsive styles and lightbox options

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*************************
+My scripts for FEWD - 4
+Robert Manolis
+Milwaukie, OR - 4/11/2017
+*************************/
+
+(function(){
+	"use strict";
+	
+	// Lightbox Options
+	lightbox.option({
+		'alwaysShowNavOnTouchDevices': true,
+		'resizeDuration': 200,
+		'wrapAround': true
+	})
+	
+
+}());
