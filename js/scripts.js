263ae37d431eb9b3509ffa4d72f6030705fceaee
Robert Manolis <grobertpm@gmail.com>
2017-04-12 23:58:58 -0700

    added search functionality and fine tuned styles

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,31 @@ Milwaukie, OR - 4/11/2017
 		'alwaysShowNavOnTouchDevices': true,
 		'resizeDuration': 200,
 		'wrapAround': true
-	})
+	});
 	
-
+	
+	/**********************
+	       SEARCH
+	**********************/
+	var cerch = $("input");
+	
+	// perform search when user types into input
+	cerch.keyup(function() {
+		let val = cerch.val().toLowerCase();
+		let imgs = $(".example-image-link");
+		
+		// iterate over items
+		imgs.each(function(indy, vally) {
+			
+			// get subtitle - lowercase
+			var subtitle = $(vally).attr("data-title").toLowerCase();
+			
+			// show matches
+			if (subtitle.includes(val)) {
+				$(imgs[indy]).show(750);
+			} else {
+				$(imgs[indy]).hide(750);
+			}
+		});
+	});
 }());
